ab48f32a2c9197053df4840f2800986ee73d56a5
Gallae <71407574+Gallae@users.noreply.github.com>
2026-04-19 11:31:19 -0400

    made movement functionality
    tied childTarget to an offset position relative to the player, added walk and duck inputs to player

---
+++ TightropeWalker/Assets/Scripts/childTarget.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class childTarget : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(10,-13,20);
+    }
+}
---
+++ TightropeWalker/Assets/Scripts/testMover.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class testMover : MonoBehaviour
+{
+    public Vector3 newPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("moving");
+            newPos = transform.position += new Vector3(0, 0, 2) * Time.deltaTime;
+            transform.position = newPos;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.eulerAngles = new Vector3(90,0,0);
+            transform.position = transform.position + new Vector3(0,-1,0);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.position = transform.position + new Vector3(0, 1, 0);
+        }
+    }
+}
