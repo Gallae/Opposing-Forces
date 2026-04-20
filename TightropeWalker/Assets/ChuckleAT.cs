b9209913d4abb54a583fc52bf9c71de1f4ea6a28
Gallae <71407574+Gallae@users.noreply.github.com>
2026-04-19 20:28:20 -0400

    trying to fix laugh animation not playing

--- TightropeWalker/Assets/ChuckleAT.cs
+++ TightropeWalker/Assets/ChuckleAT.cs
@@ -8,12 +8,12 @@ namespace NodeCanvas.Tasks.Actions {
 
 	public class ChuckleAT : ActionTask {
 
-		Vector3 laughPos;
-		
+		Vector3 laughPos = new Vector3(0, 0.5f, 0);
+		public int laughCounter = 0;
+
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
         protected override string OnInit() {
-			laughPos = new Vector3(0, 0.5f, 0);
 			return null;
 		}
 
@@ -21,25 +21,26 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			//EndAction(true);
+            if (laughCounter % 2 == 0)
+            {
+                Debug.Log("up");
+                agent.transform.position += laughPos;
+             }
+            else
+            {
+                Debug.Log("down");
+                agent.transform.position -= laughPos;
+            }
+			laughCounter++;
+			if (laughCounter >= 21)
+			{
+				EndAction(true);
+			}
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			for (int i = 0; i < 20; i++)
-			{
-				if (i < 10)
-				{
-					Debug.Log("up");
-                    //agent.transform.position += laughPos;
-                    agent.transform.position = UnityEngine.Vector3.MoveTowards(agent.transform.position, (agent.transform.position + laughPos), 1f);
-                }
-                else
-                {
-                    Debug.Log("down");
-                    agent.transform.position = UnityEngine.Vector3.MoveTowards(agent.transform.position, (agent.transform.position - laughPos), 1f);
-                }
-            }
+
         }
 
 		//Called when the task is disabled.
